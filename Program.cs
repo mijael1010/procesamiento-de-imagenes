24f51ccb92ce2a70e90128efe04b1024f730ceee
mija1010 <mijael10elmer@gmail.com>
2024-02-15 19:52:03 -0400

    primera prueba git

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace khgi
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int o = 0;
+            int e = 1;
+         
+
+            Console.WriteLine("Numero");
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Resultado");
+
+            for (int i = 0; i < n; i++)
+            {
+
+                int t = o;
+
+
+                o = e;
+
+
+                e = t + o;
+
+
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
